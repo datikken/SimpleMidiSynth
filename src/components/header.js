25c39a84570d2a38c1953dcded6a87755ab53054
tikken <tikken@MacBook-Air-tikken.local>
2019-08-11 13:20:13 +0300

    initial

--- src/components/header.js
+++ src/components/header.js
@@ -17,15 +17,7 @@ const Header = ({ siteTitle }) => (
       }}
     >
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+       Midi piano demo
       </h1>
     </div>
   </header>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,67 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+const init = () => {
+  var context = new AudioContext(),
+  oscillators = {};
+
+  if (navigator.requestMIDIAccess) {
+    navigator.requestMIDIAccess()
+        .then(success, failure);
+  }
+
+  function success (midi) {
+    var inputs = midi.inputs.values();
+    // inputs is an Iterator
+
+    for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
+        // each time there is a midi message call the onMIDIMessage function
+        input.value.onmidimessage = onMIDIMessage;
+    }
+  }
+
+  function failure () {
+    console.error('No access to your midi devices.')
+  }
+
+  function onMIDIMessage (message) {
+    var frequency = midiNoteToFrequency(message.data[1]);
+
+    if (message.data[0] === 144 && message.data[2] > 0) {
+        playNote(frequency);
+        console.log(frequency);
+    }
+
+    if (message.data[0] === 128 || message.data[2] === 0) {
+        stopNote(frequency);
+    }
+  }
+
+  function midiNoteToFrequency (note) {
+    return Math.pow(2, ((note - 69) / 12)) * 440;
+  }
+
+  function playNote (frequency) {
+    oscillators[frequency] = context.createOscillator();
+    oscillators[frequency].frequency.value = frequency;
+    oscillators[frequency].connect(context.destination);
+    oscillators[frequency].start(context.currentTime);
+  }
+
+  function stopNote (frequency) {
+    oscillators[frequency].stop(context.currentTime);
+    oscillators[frequency].disconnect();
+  }
+
+}
+
+init();
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+      <h1>Hello</h1>
   </Layout>
 )
 
